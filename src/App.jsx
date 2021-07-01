9e2cd54fc072164c064307a6f1531800c24185a5
gonzalolater <gonzalolater>
2021-07-01 18:52:44 -0300

    Hago Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css';
 import Home from './components/Home'
 import Navbar from './components/Navbar/Navbar'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Contacto from './components/Contacto';
+import Servicios from './components/Servicios';
+import Mensajes from './components/Mensajes';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar/>
-      <Home/>
-    </div>
+      <Switch>
+      <Route exact path='/' component={Home}></Route>
+      <Route exact path='/contacto' component={Contacto}></Route>
+      <Route exact path='/servicios' component={Servicios}></Route>
+      <Route exact path='/mensajes' component={Mensajes}></Route>
+      </Switch>
+    </Router>
   );
 }
 
