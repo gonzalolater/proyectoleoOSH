064987a892569cd0b2dbabc8808c71961b14d6d6
gonzalolater <gonzalolater>
2021-07-03 15:19:48 -0300

    Completado de la Pagina Contacto

--- src/components/Contacto.js
+++ src/components/Contacto.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import './Navbar/styles/Foto.css'
 
 const Contacto = () => {
     return (
         <div>
-            <h1> Contacto </h1>
-
+<section id="contact" class="contact-section">
+    <div class="contact-section-header">
+      <h2>Trabajemos juntos...</h2>
+      <p>Queres tomar un cafe ?</p>
+    </div>
+    <div class="contact-links">
+      <a href="https://www.linkedin.com/in/leandro-ariel-aguilar-osh/" class="btn contact-details"
+        ><i class="fab fa-linkedin"></i>Linkedin</a
+      >
+      <a href="tel:+54 9 11 3304 8172" class="btn contact-details"
+        ><i class="fas fa-mobile-alt"></i> Llamame</a
+      >
+      <a
+        href="https://www.facebook.com/aguilar.leandro.3"
+        class="btn contact-details"
+        ><i class="fab fa-facebook-square"></i> Facebook</a
+      >
+    </div>
+  </section>
         </div>
     )
 }
